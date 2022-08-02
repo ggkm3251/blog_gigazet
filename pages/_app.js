4d8af1be826b10e899d0d7ffe050c73a10043d33
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-08-02 14:16:10 +0300

    google adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Script from 'next/script'
+
 
 import '../styles/globals.scss';
 import { Layout } from '../components';
@@ -7,15 +7,6 @@ import { Layout } from '../components';
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Script 
-      id="Adsense-id"
-      data-ad-client="ca-pub-8425895904266381"
-      async
-      strategy="afterInteractive"
-      crossorigin="anonymous"
-      onError={ (e) => { console.error('Script failed to load', e) }}
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
-     />
       <Component {...pageProps} />
     </Layout>
   );
