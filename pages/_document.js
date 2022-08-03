53254cd38caacc6bf1fae99f4124fce33f8a68cd
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-08-03 22:33:11 +0300

    Adsense

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,41 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Adsense1 from '../components/Adsense1';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
-
+  
   render() {
     return (
       <Html>
         <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
-     crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+     <script dangerouslySetInnerHTML={{
+        __html: `
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "pub-8425895904266381",
+            enable_page_level_ads: true
+            });
+            `,
+            }} />
           {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
-     crossorigin="anonymous"></script>
-<!-- Adsense1 -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-8425895904266381"
-     data-ad-slot="7348642176"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+          <Adsense1 />
+          {/*<ins class="adsbygoogle"
+              style="display:block"
+              data-ad-client="ca-pub-8425895904266381"
+              data-ad-slot="7348642176"
+              data-ad-format="auto"
+              data-full-width-responsive="true"></ins>
+          <script>
+              (adsbygoogle = window.adsbygoogle || []).push({});
+          </script>*/}
+          
         </body>
       </Html>
     );
