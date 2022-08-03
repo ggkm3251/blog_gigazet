883887ee156e3eca3da4ff3920f3fde09a4871c8
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-08-03 21:30:31 +0300

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,18 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
+     crossorigin="anonymous"></script>
+<!-- Adsense1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8425895904266381"
+     data-ad-slot="7348642176"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         </body>
       </Html>
     );
