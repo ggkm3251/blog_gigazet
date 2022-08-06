d74327f5eef3fe2cecf4204fab8ab4e39d144b5d
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-08-07 00:07:47 +0300

    netlify

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,31 @@ import React from 'react';
 
 import '../styles/globals.scss';
 import { Layout } from '../components';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script id="Adsense-id" 
+  async strategy="afterInteractive"
+  onError={ (e) => { console.error('Script failed to load', e) }}
+src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
+crossorigin="anonymous"
+/>
+<div className="col-md-8">
+    <h3 className="pb-4 mb-4 fst-italic border-bottom">
+       Adsense ads
+    </h3>
+    <ins class="adsbygoogle"
+     style={{display:block}}
+     data-ad-format="fluid"
+     data-ad-layout-key="+73+q7+1w-uv+tl"
+     data-ad-client="ca-pub-8425895904266381"
+     data-ad-slot="6232669921"></ins>
+      <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});    
+     </script>
+</div>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -10,8 +11,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8425895904266381"
-     crossorigin="anonymous"></script>
           {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
         </Head>
         <body>
